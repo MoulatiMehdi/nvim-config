89797a1dbda118c94b1c8afe684ac39954046716
Mehdi_Moulati <moulati.mehdi@gmail.com>
2024-09-13 17:01:21 +0100

    remove 42header plugin : already integrated in the nvim-norminette

--- lua/config/lazy-plugins.lua
+++ lua/config/lazy-plugins.lua
@@ -58,29 +58,46 @@ require('lazy').setup({
     {
         'MoulatiMehdi/nvim-norminette',
         config = function()
+            -- Load the Norminette plugin
             local norm = require 'norminette'
+
             norm.setup {
+                header_on_save = true,
                 format_on_save = true,
             }
-            vim.keymap.set(
-                'n',
-                '<F5>',
-                ':Norminette<CR>',
-                { noremap = true, silent = true }
-            )
+            vim.keymap.set('n', '<F5>', function()
+                norm.check_norms()
+            end, {
+                desc = 'Update 42norms diagnostics',
+                noremap = true,
+                silent = true,
+            })
 
-            vim.keymap.set(
-                'n',
-                '<C-f>',
-                ':Format<CR>',
-                { noremap = true, silent = true }
-            )
-        end,
-    },
-    {
-        'MoulatiMehdi/42header.nvim',
-        config = function()
-            require '42header'
+            vim.keymap.set('n', '<C-f>', function()
+                norm.format()
+            end, {
+                desc = 'Format buffer on 42norms',
+                noremap = true,
+                silent = true,
+            })
+
+            vim.keymap.set('n', '<F1>', function()
+                norm.stdheader()
+            end, {
+                desc = 'Insert 42header',
+                noremap = true,
+                silent = true,
+            })
+
+            vim.api.nvim_create_user_command('Norminette', function()
+                norm.check_norms()
+            end, {})
+            vim.api.nvim_create_user_command('Format', function()
+                norm.format()
+            end, {})
+            vim.api.nvim_create_user_command('Stdheader', function()
+                norm.stdheader()
+            end, {})
         end,
     },
     {

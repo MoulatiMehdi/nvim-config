73056dcb8c3a1fd5566c1d94092b2dcd666b15c4
MoulatiMehdi <moulati.mehdi@gmail.com>
2024-11-29 10:21:29 +0100

    add git treesitter

--- init.lua
+++ init.lua
@@ -2,12 +2,13 @@ vim.g.mapleader = " "
 vim.g.have_nerd_front = true
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 
+require "configs.lazy-bootstrap"
+require "configs.lazy-plugins"
+
 -- load theme
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
-require "configs.lazy-bootstrap"
-require "configs.lazy-plugins"
 require "nvchad.autocmds"
 
 vim.schedule(function()
--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -1,26 +1,31 @@
+---@type vim.treesitter.languagetree.Injection
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			build = ":TSUpdate",
-			auto_install = true,
-			ensure_installed = {
-				"markdown",
-				"markdown_inline",
-				"query",
-				"diff",
-				"vim",
-				"lua",
-				"luadoc",
-				"vimdoc",
-				"c",
-				"bash",
-			},
-			indent = { enable = true, disable = { "ruby" } },
-		},
-		config = function(_, opts)
-			require("nvim-treesitter.install").prefer_git = true
-			require("nvim-treesitter.configs").setup(opts)
-		end,
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            build = ":TSUpdate",
+            auto_install = true,
+            ensure_installed = {
+                "git_config",
+                "git_rebase",
+                "gitattributes",
+                "gitcommit",
+                "markdown",
+                "markdown_inline",
+                "query",
+                "diff",
+                "vim",
+                "lua",
+                "luadoc",
+                "vimdoc",
+                "c",
+                "bash",
+            },
+            indent = { enable = true, disable = { "ruby" } },
+        },
+        config = function(_, opts)
+            require("nvim-treesitter.install").prefer_git = true
+            require("nvim-treesitter.configs").setup(opts)
+        end,
+    },
 }
